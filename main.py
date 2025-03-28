6390f09f34f1cd972aac3e601c1b7b37d6ffc441
TakahashiNatsuki <130946063+TakahashiNatsuki@users.noreply.github.com>
2025-03-29 03:54:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ import os
 st.set_page_config(page_title="健康体型学習 入力フォーム", layout="centered")
 st.title("健康体型学習 入力フォーム")
 
-# 入力欄
 user_id = st.text_input("指定のIDを入力してください。")
 age = st.number_input("年齢（18〜64歳）", min_value=18, max_value=64)
 sex = st.selectbox(
@@ -47,14 +46,15 @@ if st.button("送信する"):
         "bmr": round(bmr, 2)
     }
 
+    SAVE_DIR = "C:\Users\hdari\DietEducation_Prototype\UserDataFile"
+    os.makedirs(SAVE_DIR, exist_ok=True)
     df = pd.DataFrame([data])
-    os.makedirs("userdata", exist_ok=True)
-    df.to_csv(f"C:\Users\hdari\DietEducation_Prototype\UserDataFile/userdata_{user_id}.csv", index=False, encoding='utf-8-sig')
-    with open(f"C:\Users\hdari\DietEducation_Prototype\UserDataFile/userdata_{user_id}.json", "w", encoding="utf-8") as f:
+    df.to_csv(os.path.join(SAVE_DIR, f"userdata_{user_id}.csv"), index=False, encoding='utf-8-sig')
+    with open(os.path.join(SAVE_DIR, f"userdata_{user_id}.json"), "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=2)
 
     st.session_state["user_id"] = user_id
     st.session_state["bmr"] = round(bmr, 2)
 
     st.success("データが保存されました。")
-    st.info("画面左のサイドバーから「事前アンケート・テスト」ページに移動してください。")
+    st.info("画面左のサイドバーから「アンケート・テスト（前）」ページに移動してください。")
